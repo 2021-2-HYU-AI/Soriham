536e3d8f07c89daa94087a0dd2ad6653862dc9cd
Jun-gi Kim <damnum3@hanyang.ac.kr>
2021-11-25 02:21:37 +0900

    feat: default navigation

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './screens/Login';
+import Login from './screens/main/Login'
+import Main from './screens/main/Main';
+import VoiceSetting from './screens/voice/VoiceSetting';
+import VoiceSettingDetail from './screens/voice/VoiceSettingDetail';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Login />
-      {/* <Main /> */}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* <Stack.Screen name="Login" component={Login} 로그인 기능 */}
+        <Stack.Screen name="Main" component={Main} />
+        {/* <Stack.Screen name="Message" component={SendMessage} /> 메시지 보내기 기능 */}
+        <Stack.Screen name="Voice Setting" component={VoiceSetting} />
+        <Stack.Screen name="Detail Setting" component={VoiceSettingDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
---
+++ screens/main/Main.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { AntDesign } from '@expo/vector-icons'; 
+import { 
+    View,
+    Text,
+    StatusBar,
+    StyleSheet,
+    TouchableOpacity,
+    SafeAreaView
+    } from "react-native";
+
+const Main = ({navigation}) => {
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar style="auto" />
+            <View style={styles.header}>
+                <Text style={styles.headerText}>윤석님, 환영합니다.</Text>
+            </View>
+
+            <View style={styles.menuListWrapper}>
+                <View style={styles.menuWrapper}>
+                    <View style={styles.menuTextWrapper}>
+                        <Text style={styles.menuTitle}>메시지 보내기</Text>
+                        <Text style={styles.menuText}>텍스트로 보유하신 스피커에 메시지를 남길 수 있습니다.</Text>
+                    </View>
+                    <TouchableOpacity>
+                        <AntDesign name="right" size={24} color="black" />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.menuWrapper}>
+                    <View style={styles.menuTextWrapper}>
+                        <Text style={styles.menuTitle}>보이스 설정</Text>
+                        <Text style={styles.menuText}>스피커에서 메시지를 출력 시 사용할 보이스를 설정할 수 있습니다.</Text>
+                    </View>
+                    <TouchableOpacity onPress={() => navigation.navigate('Voice Setting')}>
+                        <AntDesign name="right" size={24} color="black" />
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        flex: 1,
+        justifyContent: "center",
+        paddingHorizontal: 20,
+        borderBottomColor: "black",
+        borderBottomWidth: 1,
+    },
+    headerText: {
+        fontSize: 20,
+        fontWeight: "500",
+    },
+    menuListWrapper: {
+        flex: 10,
+    },
+    menuWrapper: {
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection: "row",
+        padding: 20,
+        borderBottomColor: "gray",
+        borderBottomWidth: 1,
+    },
+    menuTextWrapper: {
+
+    },
+    menuTitle: {
+        fontSize: 15,
+    },
+    menuText: {
+        fontSize: 11,
+        color: "gray",
+    },
+});
+
+export default Main;
