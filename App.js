08c250c404e368025cc94e475cbeeef3a429af1f
raonepark <raonepark@gmail.com>
2021-12-05 01:22:23 +0900

    프론트앤드 완성

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Login from './screens/main/Login'
 import Main from './screens/main/Main';
+import Chat from './screens/main/Chat'
 import VoiceSetting from './screens/voice/VoiceSetting';
 import VoiceSettingDetail from './screens/voice/VoiceSettingDetail';
 import { NavigationContainer } from '@react-navigation/native';
@@ -9,16 +10,23 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="Login" component={Login} 로그인 기능 */}
-        <Stack.Screen name="Main" component={Main} />
-        {/* <Stack.Screen name="Message" component={SendMessage} /> 메시지 보내기 기능 */}
-        <Stack.Screen name="Voice Setting" component={VoiceSetting} />
-        <Stack.Screen name="Detail Setting" component={VoiceSettingDetail} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Main" component={Main} />
+      <Stack.Screen name="VoiceSetting" component={VoiceSetting} />
+      <Stack.Screen name="VoiceSettingDetail" component={VoiceSettingDetail} />
+      <Stack.Screen name="Chat" component={Chat} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+
+// <Stack.Screen name="Main" component={Main} />
+// <Stack.Screen name="Message" component={SendMessage} /> 메시지 보내기 기능 */}
+// <Stack.Screen name="Voice Setting" component={VoiceSetting} />
+// <Stack.Screen name="Detail Setting" component={VoiceSettingDetail} />
